803a65acaa5968011bdf5f8c4e6d299c93c0324a
Maria Chikhireva <mchikhireva@gmail.com>
2025-02-20 21:07:37 +0300

    add create article form

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from hexlet_django_blog.article.models import Article
+from hexlet_django_blog.article.forms import ArticleForm
 
 class IndexView(View):
 
@@ -15,3 +16,16 @@ class ArticleView(View):
     def get(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['id'])
         return render(request, 'articles/show.html', context={'article':article})
+
+
+class ArticleFormCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form':form})
+    
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            return redirect('articles') 
+        return render(request, 'articles/create.html', {'form': form})
